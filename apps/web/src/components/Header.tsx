f5739c0cc721fd615bfc505abcd9e8617a44842a
Turbobot <turbobot@vercel.com>
2024-04-27 13:58:07 +0700

    feat(create-turbo): apply official-starter transform

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,13 @@
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="h-[150px] w-[10rem] bg-[#F40841] text-red-500">
+      <h1 className="text-red-500">Hello World</h1>
+      <div
+        className="bg-red-500 size-[10rem]
+      "
+      >
+        kotak
+      </div>
+    </section>
+  );
 };
