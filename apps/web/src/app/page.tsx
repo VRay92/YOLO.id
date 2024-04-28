3a76be945a84287458096d8088b5a91e4f6283e1
VRay92 <adrian.irawan92@gmail.com>
2024-04-28 14:48:05 +0700

    fix: git stash

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,15 @@
+'use client';
 import Image from 'next/image';
 import styles from './page.module.css';
+import { useState } from 'react';
+import Toptrending from '@/components/Toptrending';
 
 export default function Home() {
+  const [select, setSelect] = useState('music');
   return (
-    <main className="w-full">
+    <main className="w-full mt-[75px] md:mt-[120px]">
+      {/* desktop view hero section */}
+
       <div
         id="background-web"
         className="relative hidden h-[660px] w-full md:block "
@@ -26,7 +32,92 @@ export default function Home() {
           ></Image>
         </div>
       </div>
-      <h1 className="text-white text-[50px] font-bold font-poppins">Events</h1>
+
+      {/* mobile view hero section */}
+
+      <div
+        id="background-web"
+        className="relative block h-[265px] w-full md:hidden "
+      >
+        <Image
+          fill
+          sizes="100vw"
+          src="/hero-section.jpg"
+          alt="hero"
+          className="object-cover"
+        ></Image>
+        <div className="w-[343px] h-[170px] absolute left-[8%] bottom-[15%]">
+          {' '}
+          <Image
+            fill
+            sizes="100vw"
+            src="/anggun.png"
+            alt="hero"
+            className="object-cover rounded-lg"
+          ></Image>
+        </div>
+      </div>
+
+      {/* content */}
+
+      <h1 className="text-white text-[40px] font-bold font-poppins ml-5 md:mx-16 mt-4 md:mt-10">
+        Events
+      </h1>
+      <div className="mt-0 ml-5 overflow-x-scroll no-scrollbar md:mt-4 md:mx-16">
+        <ul className="flex gap-4 text-lg text-white md:gap-10">
+          <li
+            className={`cursor-pointer ${
+              select === 'music' ? 'border-b-2 border-white' : ''
+            }`}
+            onClick={() => setSelect('music')}
+          >
+            Music
+          </li>
+          <li
+            className={`cursor-pointer ${
+              select === 'festival' ? 'border-b-2 border-white' : ''
+            }`}
+            onClick={() => setSelect('festival')}
+          >
+            Festival&Bazaar
+          </li>
+          <li
+            className={`cursor-pointer ${
+              select === 'sports' ? 'border-b-2 border-white' : ''
+            }`}
+            onClick={() => setSelect('sports')}
+          >
+            Sports
+          </li>
+          <li
+            className={`cursor-pointer ${
+              select === 'expo' ? 'border-b-2 border-white' : ''
+            }`}
+            onClick={() => setSelect('expo')}
+          >
+            Exhibition&Expo
+          </li>
+          <li
+            className={`cursor-pointer ${
+              select === 'seminar' ? 'border-b-2 border-white' : ''
+            }`}
+            onClick={() => setSelect('seminar')}
+          >
+            Seminar
+          </li>
+        </ul>
+      </div>
+      <div className="h-[380px] w-full bg-[#989898] mt-10 relative">
+        {' '}
+        <h1 className="absolute text-5xl font-bold origin-right transform -rotate-90 -left-60">
+          Top Trending
+        </h1>
+        <div className="flex ml-40">
+          <Toptrending url="/anggun.png"></Toptrending>
+          <Toptrending url="/LiSa.jpg"></Toptrending>
+          <Toptrending url="/judika.jpg"></Toptrending>
+        </div>
+      </div>
     </main>
   );
 }
--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image';
 import { SlMagnifier } from 'react-icons/sl';
 import { BsPersonCircle } from 'react-icons/bs';
 import { BsFillTicketPerforatedFill } from 'react-icons/bs';
+import { IoMenu } from 'react-icons/io5';
 
 export const Header = () => {
   return (
-    <nav className=" w-full ">
-      <div className="flex justify-end pr-16 h-[40px] bg-[#D9D9D9] py-2 text-[#2c2c2c]">
+    <nav className="fixed top-0 z-10 w-full ">
+      {/* top navbar */}
+
+      <div className="hidden md:flex justify-end md:pr-16 h-[40px] bg-[#D9D9D9] py-2 ">
         <span className="px-4">
           <h1>About YOLO</h1>
         </span>
@@ -18,30 +21,48 @@ export const Header = () => {
           <h1>Contact Us</h1>
         </span>
       </div>
-      <div className="flex h-[100px] bg-[#F40841] justify-between">
+
+      {/* red navbar */}
+
+      <div className="flex h-[75px] md:h-[90px] bg-[#F40841]  justify-between">
         <div className="flex">
-          <div className="my-auto ml-20 mr-[121px]">
+          <div className="my-auto ml-20 mr-[121px] hidden md:block">
             <Image
-              width={173}
-              height={78}
+              width={160}
+              height={72}
               src="/logo_white.png"
               alt="logo"
             ></Image>
           </div>
-          <span className="w-[50px] h-[50px] bg-white my-auto rounded-l-md">
-            <SlMagnifier className="m-auto my-3 text-2xl" />
-          </span>
-          <input
-            type="text"
-            className="w-[685px] h-[50px] bg-[#d9d9d9] my-auto pl-10 rounded-r-md"
-            placeholder="search here.."
-          />
+          <div className="block my-auto ml-6 md:hidden">
+            <Image
+              width={119}
+              height={54}
+              src="/logo_white.png"
+              alt="logo"
+            ></Image>
+          </div>
+          {/* search bar */}
+          <div className="hidden md:flex">
+            <span className="w-[50px] h-[40px] bg-white my-auto rounded-l-md">
+              <SlMagnifier className="m-auto my-2 text-2xl" />
+            </span>
+            <input
+              type="text"
+              className="flex pl-10 my-auto  rounded-r-md w-[685px] h-[40px] bg-[#d9d9d9]"
+              placeholder="search here.."
+            />
+          </div>
         </div>
-        <div className="flex mr-20 my-auto font-poppins text-[#2c2c2c]">
-          <div className="my-1 text-4xl mr-3">
+        <div className="flex gap-4 my-auto mr-4 text-3xl text-white md:hidden">
+          <SlMagnifier className="" />
+          <IoMenu />
+        </div>
+        <div className="hidden my-auto mr-20 text-white md:flex font-poppins">
+          <div className="my-1 mr-3 text-4xl">
             <BsFillTicketPerforatedFill></BsFillTicketPerforatedFill>
           </div>
-          <span className="text-lg mr-16 my-2">My Tickets</span>
+          <span className="my-2 mr-16 text-lg">My Tickets</span>
           <div className="text-5xl">
             <BsPersonCircle></BsPersonCircle>
           </div>
